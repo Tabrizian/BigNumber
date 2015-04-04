8fdb7f989744bcb7e85b722c668aea1c94760d87
Iman Tabrizian <Tabrizian@outlook.com>
2015-04-04 16:30:14 +0430

    First init with big integer sum method

---
+++ BigNumber/src/BigNumber.java
@@ -0,0 +1,43 @@
+public class BigNumber {
+	private String number;
+
+	public BigNumber(String number) {
+		StringBuilder strBuild = new StringBuilder(number);
+		this.number = strBuild.reverse().toString();
+	}
+
+	public int getAt(int i) {
+		return Integer.parseInt(number.charAt(i) + "");
+	}
+
+	public BigNumber add(BigNumber num2) {
+		StringBuilder num = new StringBuilder();
+		num.reverse();
+		int remain = 0;
+		for (int i = 0; i < number.length(); i++) {
+			int number1 = 0;
+			try {
+				number1 = this.getAt(i);
+			} catch (Exception e) {
+			}
+			int number2 = 0;
+			try {
+				number2 = num2.getAt(i);
+			} catch (Exception e) {
+			}
+			int temp = number1 + number2 + remain;
+			remain = temp / 10;
+			temp = temp % 10;
+
+			num.append(temp);
+		}
+		if (remain > 0)
+			num.append(remain);
+		return new BigNumber(num.toString());
+	}
+
+	@Override
+	public String toString() {
+		return number;
+	}
+}
---
+++ BigNumber/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BigNumber a = new BigNumber("99999999");
+		BigNumber b = new BigNumber("1");
+		System.out.println(a.add(b));
+
+	}
+
+}
