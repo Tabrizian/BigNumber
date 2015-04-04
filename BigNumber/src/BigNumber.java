32952fd1e67b957963cf6ea9d082df4b3d36e28d
Iman Tabrizian <Tabrizian@outlook.com>
2015-04-04 16:36:14 +0430

    Subtract method added!

--- BigNumber/src/BigNumber.java
+++ BigNumber/src/BigNumber.java
@@ -35,6 +35,46 @@ public class BigNumber {
 			num.append(remain);
 		return new BigNumber(num.toString());
 	}
+	
+	public BigNumber subtract(BigNumber num2) {
+		StringBuilder num = new StringBuilder();
+		num.reverse();
+		int remain = 0;
+		boolean subtracted = false;
+		for (int i = 0; i < number.length(); i++) {
+			
+			int number1 = 0;
+			try {
+				number1 = this.getAt(i);
+			} catch (Exception e) {
+			}
+			int number2 = 0;
+			try {
+				number2 = num2.getAt(i);
+			} catch (Exception e) {
+			}
+			
+			if (subtracted) {
+				number1--;
+				subtracted = false;
+			}
+			int temp = number1 - number2 + remain;
+			
+			if(temp < 0){
+				subtracted = true;
+				temp += 10;
+			}
+				
+			remain = temp / 10;
+			temp = temp % 10;
+
+			num.append(temp);
+		}
+		if (remain > 0)
+			num.append(remain);
+		return new BigNumber(num.toString());
+	}
+	
 
 	@Override
 	public String toString() {
--- BigNumber/src/Main.java
+++ BigNumber/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		BigNumber a = new BigNumber("99999999");
-		BigNumber b = new BigNumber("1");
-		System.out.println(a.add(b));
+		BigNumber a = new BigNumber("99999998");
+		BigNumber b = new BigNumber("9");
+		System.out.println(a.subtract(b));
 
 	}
 
