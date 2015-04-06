727b424fbd6e30eeade2b5a8dd1d8df7326ad0ca
Iman Tabrizian <Tabrizian@outlook.com>
2015-04-06 19:12:23 +0430

    Commands support < , > and ! operators

--- BigNumber/src/Input.java
+++ BigNumber/src/Input.java
@@ -5,16 +5,30 @@ public class Input {
 		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
 		String[] args;
-		BigNumber ans = null;
+		String ans = null;
 		if (s.indexOf('+') != -1) {
 			args = s.split("+");
-			ans = new BigNumber(args[0]).manage(new BigNumber(args[1]), '+');
-		} else if(s.indexOf('-') != -1) {
+			ans = new BigNumber(args[0]).manage(new BigNumber(args[1]), '+')
+					.toString();
+		} else if (s.indexOf('-') != -1) {
 			args = s.split("-");
-			ans = new BigNumber(args[0]).manage(new BigNumber(args[1]), '-');
-		} else if(s.indexOf('*') != -1) {
+			ans = new BigNumber(args[0]).manage(new BigNumber(args[1]), '-')
+					.toString();
+		} else if (s.indexOf('*') != -1) {
 			args = s.split("*");
-			ans = new BigNumber(args[0]).manage(new BigNumber(args[1]), '*');
+			ans = new BigNumber(args[0]).manage(new BigNumber(args[1]), '*')
+					.toString();
+		} else if (s.indexOf('!') != -1) {
+			args = s.split("!");
+			ans = String.valueOf((new BigNumber(args[1]).isZero()));
+		} else if (s.indexOf('<') != -1) {
+			args = s.split("<");
+			ans = String.valueOf(new BigNumber(args[0]).compare((new BigNumber(
+					args[1]))));
+		} else if (s.indexOf('>') != -1) {
+			args = s.split(">");
+			ans = String.valueOf(new BigNumber(args[1]).compare((new BigNumber(
+					args[0]))));
 		}
 		return ans.toString();
 	}
